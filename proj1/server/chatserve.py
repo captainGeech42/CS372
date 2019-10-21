124b1996832ce935b8571dfea18ae0c2eb5314e6
Alexander Nead-Work <neadwora@oregonstate.edu>
2019-10-20 23:46:10 -0700

    server v0.1 done

--- proj1/server/chatserve.py
+++ proj1/server/chatserve.py
@@ -5,7 +5,19 @@ import sys
 from server import Server
 
 def main(argv):
-    s = Server("asdf", 10001)
+    if len(argv) != 2:
+        print(f"[!] usage: {argv[0]} <port>")
+        return 1
+
+    try:
+        port = int(argv[1])
+        assert(port >= 1024)
+        assert(port < 65535)
+    except:
+        print("[!] invalid port specified (must be int between 1024 and 65535")
+        return 1
+
+    s = Server(port)
 
     return 0
 
--- proj1/server/server.py
+++ proj1/server/server.py
@@ -1,9 +1,59 @@
+import socket
+
+username = "i_am_groot"
+prompt = f"{username}> "
+max_buf = 513
+
 class Server:
-    def __init__(self, host, port):
-        self.host = host
+    def __init__(self, port: int):
         self.port = port
+        self.host = socket.gethostname()
 
         self.start()
 
     def start(self):
-        print("starting")
+        """Start the chat server"""
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            self.socket.bind((self.host, self.port))
+            self.socket.listen(1)
+            print(f"[*] server listening on {self.host}:{self.port}")
+        except:
+            print("[!] error spawning server, try different port?")
+
+        conn, addr = self.socket.accept() # blocking
+        print(f"[*] got connection from {addr}")
+
+        handle(conn)
+
+    def handle(self, conn: socket):
+        """Handle a client socket connection"""
+
+        # client sends first message
+        while True:
+            data = conn.receive(max_buf) # blocking
+            
+            if "\quit" in data:
+                print("[*] closing (requested by client)")
+                conn.close()
+                break
+
+            print(data)
+
+            msg = get_input()
+            
+            if msg == "\quit":
+                print("[*] closing (requested by server)")
+                conn.close()
+                break
+
+            if len(msg) > 500:
+                print("[!] msg longer than 500 bytes, truncating")
+                msg = msg[:500]
+            data = prompt + msg
+            conn.sendall(data)
+        
+    def get_input(self):
+        return input(prompt)

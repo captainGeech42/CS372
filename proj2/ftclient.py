4e77ffc4d75f55717f15260625592af3044de4b1
Alexander Nead-Work <neadwora@oregonstate.edu>
2019-11-10 12:32:33 -0800

    started proj2

---
+++ proj2/ftclient.py
@@ -0,0 +1,7 @@
+"""
+Program: ftclient (ftclient.py)
+Description: Connect to a TCP server for a file transfer system
+Written by: Alexander Nead-Work (neadwora)
+Course: CS 372-400 (Fall 2019, Redfield)
+Last Modified: 2019 Nov 10
+"""
---
+++ proj2/server.c
@@ -0,0 +1,7 @@
+/*
+Program: ftserver (server.c)
+Description: Spawn a TCP server for a file transfer system
+Written by: Alexander Nead-Work (neadwora)
+Course: CS 372-400 (Fall 2019, Redfield)
+Last Modified: 2019 Nov 10
+*/
